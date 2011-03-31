6080e2808256a77a979b16ca5e50dbb7b25e036c
erhun.baycelik@gmail.com <erhun.baycelik@gmail.com@46466cab-7080-a701-ecfc-22f6a81af26d>
2011-03-31 21:48:02 +0000

    Add Lecturer fonksiyonu LecturerBean'e eklendi.

--- MyCoursesProject/src/entities/business/LecturerBean.java
+++ MyCoursesProject/src/entities/business/LecturerBean.java
@@ -1,5 +1,24 @@
 package entities.business;
 
+import entities.dao.Lecturer;
+
 public class LecturerBean {
+	private String lecturerName;
+	private Lecturer lecturer = new Lecturer();
+	
+	public String AddLecturer(){
+		lecturer.setLecturerName(lecturerName);
+		lecturer.AddLecturer();
+		return null;
+	}
+
+	public String getLecturerName() {
+		return lecturerName;
+	}
+
+	public void setLecturerName(String lecturerName) {
+		this.lecturerName = lecturerName;
+	}
+	
 	
 }
