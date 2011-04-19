59de734259a21215778a2448bd1654972975be0b
erdiapaydin@gmail.com <erdiapaydin@gmail.com@46466cab-7080-a701-ecfc-22f6a81af26d>
2011-04-19 21:32:36 +0000

    Validator paketi eklendi.

---
+++ MyCoursesProject/src/entities/validator/EmailValidator.java
@@ -0,0 +1,23 @@
+package entities.validator;
+import java.util.regex.*;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+public class EmailValidator implements Validator{
+	
+		public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+			String email = (String) value;
+			if(!email.contains("@")) {
+			FacesMessage message = new FacesMessage();
+			message.setSeverity(FacesMessage.SEVERITY_ERROR);
+			message.setSummary("Email is not valid.");
+			message.setDetail("Email is not valid.");
+			context.addMessage("userForm:Email", message);
+			throw new ValidatorException(message);
+			}
+		}
+	}
