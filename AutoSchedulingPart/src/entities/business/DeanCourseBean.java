b05b406948fe6971da0136db88e02cec3c343c03
onurozey@gmail.com <onurozey@gmail.com@46466cab-7080-a701-ecfc-22f6a81af26d>
2011-04-27 19:47:08 +0000

    Initializing...

---
+++ AutoSchedulingPart/src/entities/business/DeanCourseBean.java
@@ -0,0 +1,367 @@
+package entities.business;
+import java.awt.Color;
+import java.util.*;
+
+import javax.faces.event.ValueChangeEvent;
+import javax.faces.model.SelectItem;
+import javax.swing.JOptionPane;
+
+import entities.dao.Course;
+import entities.dao.Lecturer;
+
+public class DeanCourseBean 
+{
+	private ArrayList<SelectItem> deanCourseList;
+	private ArrayList<SelectItem> deanLecturerList;
+	
+	private ArrayList<Course> courseList;
+	
+	private Hashtable<String, Integer> dayMapToIndexHash;
+	
+	public String[][] initCourseTable = new String[8][6];
+
+	Course courseObj = new Course();
+	Lecturer lecturerObj = new Lecturer();
+	
+	private String selectedDeanCourse = "";
+	String selectedDeanLecturer;
+	String selectedDeanDay = "";
+	String selectedOperation = "";
+	String selectedStartHour = "";
+	String selectedEndHour = "";
+	String selectedRoom = "";
+	boolean buttonStatus = false;
+	
+	Color testColor = Color.green;
+	
+	String creditValueTheo;
+	String creditValuePrac;
+	
+	public DeanCourseBean()
+	{
+		deanCourseList = new ArrayList<SelectItem>();
+		deanCourseList.add(new SelectItem("PHY1002", "Physics II"));
+		deanCourseList.add(new SelectItem("MAT1002", "Calculus II"));
+		deanCourseList.add(new SelectItem("TUR2002", "Turk Dili"));
+		deanCourseList.add(new SelectItem("TAR1002", "Tarih II"));
+		
+		dayMapToIndexHash = new Hashtable<String, Integer>();
+		dayMapToIndexHash.put("Monday", 1);
+		dayMapToIndexHash.put("Tuesday", 2);
+		dayMapToIndexHash.put("Wednesday", 3);
+		dayMapToIndexHash.put("Thursday", 4);
+		dayMapToIndexHash.put("Friday", 5);
+		
+		initCourseTable[0][0] = "1";
+		initCourseTable[1][0] = "2";
+		initCourseTable[2][0] = "3";
+		initCourseTable[3][0] = "4";
+		initCourseTable[4][0] = "5";
+		initCourseTable[5][0] = "6";
+		initCourseTable[6][0] = "7";
+		initCourseTable[7][0] = "8";
+				
+			
+	}
+	
+	public String clearDeanCourseTable()
+	{
+		try
+		{
+			initCourseTable = new String[8][6];
+			initCourseTable[0][0] = "1";
+			initCourseTable[1][0] = "2";
+			initCourseTable[2][0] = "3";
+			initCourseTable[3][0] = "4";
+			initCourseTable[4][0] = "5";
+			initCourseTable[5][0] = "6";
+			initCourseTable[6][0] = "7";
+			initCourseTable[7][0] = "8";
+		}
+		catch(Exception ex)
+		{
+			System.out.println(ex.getMessage());
+		}
+		return null;
+	}
+	
+	List<SelectItem> departmentFreshmanCourseList = new ArrayList<SelectItem>();
+	List<Course> deptFreshmanCourseList = null;
+	public List<SelectItem> getDepartmentCourseList()
+	{
+		synchronized (this) 
+		{
+			if(deptFreshmanCourseList == null)
+			{
+				try
+				{
+					deptFreshmanCourseList = courseObj.getDepartmentCourseNameList();
+					for(int i = 0; i < deptFreshmanCourseList.size(); i++)
+					{
+						String deptCourseName = deptFreshmanCourseList.get(i).getCourseName();
+						departmentFreshmanCourseList.add(new SelectItem(deptCourseName));
+					}
+				}
+				catch(Exception ex)
+				{
+					ex.getMessage();
+				}
+			}
+		}
+		return departmentFreshmanCourseList;
+	}
+	
+	public String initDeanCourseTable()
+	{
+		try
+		{			
+			
+			if(selectedOperation.equals("Theory Operation"))
+			{
+				System.out.println("Function 2*****");
+				if(!selectedDeanDay.equals("Choose Days"))
+				{
+					
+					if(!selectedStartHour.equals("Choose Start Hour") && !selectedEndHour.equals("Choose End Hour"))
+					{
+						
+						int dayIndexOnCourseTable = dayMapToIndexHash.get(selectedDeanDay);
+						int startHourOfDeanCourse = Integer.parseInt(selectedStartHour);
+						int endHourOfDeanCourse = Integer.parseInt(selectedEndHour);
+						
+						//int totalCourseHour = endHourOfDeanCourse - startHourOfDeanCourse;
+						
+						int startHourT = startHourOfDeanCourse - 1;
+						int endHourT = endHourOfDeanCourse - 1;
+						
+						//for(int acc = 0; acc < totalCourseHour + 1; acc++)
+						//{	
+							if(initCourseTable[startHourT][dayIndexOnCourseTable] == null)
+							{
+								if(initCourseTable[endHourT][dayIndexOnCourseTable] == null)
+								{
+									initCourseTable[startHourT][dayIndexOnCourseTable] = selectedDeanCourse + "(T)";
+									initCourseTable[endHourT][dayIndexOnCourseTable] = selectedDeanCourse + "(T)";
+									//System.out.println("Test Result ::: "+initCourseTable[startHourOfDeanCourse][dayIndexOnCourseTable]);							
+								}
+							}
+							else
+							{
+								System.out.println("Indices are not available");
+							}
+						//}
+						System.out.println(initCourseTable);
+					}
+				}
+			}		
+			else if(selectedOperation.equals("Practice Operation"))
+			{
+				if(!selectedDeanDay.equals("Choose Days"))
+				{
+					if(!selectedStartHour.equals("Choose Start Hour") && !selectedEndHour.equals("Choose End Hour"))
+					{
+						int dayIndexOnCourseTable = dayMapToIndexHash.get(selectedDeanDay);
+						int startHourOfDeanCourse = Integer.parseInt(selectedStartHour);
+						int endHourOfDeanCourse = Integer.parseInt(selectedEndHour);
+						
+						//int totalCourseHour = endHourOfDeanCourse - startHourOfDeanCourse;
+						
+						int startHourT = startHourOfDeanCourse - 1;
+						int endHourT = endHourOfDeanCourse - 1;
+						
+						//for(int acc = 0; acc < totalCourseHour + 1; acc++)
+						//{	
+							if(initCourseTable[startHourT][dayIndexOnCourseTable] == null)
+							{
+								if(initCourseTable[endHourT][dayIndexOnCourseTable] == null)
+								{
+									initCourseTable[startHourT][dayIndexOnCourseTable] = selectedDeanCourse + "(P)";
+									initCourseTable[endHourT][dayIndexOnCourseTable] = selectedDeanCourse + "(P)";
+									//System.out.println("Test Result ::: "+initCourseTable[startHourOfDeanCourse][dayIndexOnCourseTable]);							
+								}
+							}
+							else
+							{
+								System.out.println("Indices are not available");
+							}
+						//}
+					}
+				}
+			}
+		}
+		catch(Exception ex)
+		{
+			System.out.println(ex.getMessage());
+		}
+		return null;
+	}
+	
+	public void handleValueChange(ValueChangeEvent event)
+	{
+		System.out.println("Course Code : " + event.getComponent().getId());
+		String oldValue = (String)event.getOldValue();
+		String newValue = (String)event.getNewValue();
+		System.out.println("Old Value : "+oldValue);
+		System.out.println("New Value : "+newValue);
+		this.selectedDeanCourse = newValue;
+		loadLecturer();
+	}
+	
+	private void loadLecturer()
+	{
+		ArrayList<Lecturer> lctList = lecturerObj.getLecturerByCourseName(this.selectedDeanCourse);
+		ArrayList<SelectItem> itemList = new ArrayList<SelectItem>();
+		for(int i = 0; i < lctList.size(); i++) {
+			String name = lctList.get(i).getLecturerName();
+			itemList.add(new SelectItem(name));
+		}
+		this.deanLecturerList = itemList;
+	}
+	
+	public List<Course> getDeptFreshmanCourseList() {
+		return deptFreshmanCourseList;
+	}
+
+	public void setDeptFreshmanCourseList(List<Course> deptFreshmanCourseList) {
+		this.deptFreshmanCourseList = deptFreshmanCourseList;
+	}
+
+	public List<SelectItem> getDepartmentFreshmanCourseList() {
+		return departmentFreshmanCourseList;
+	}
+
+	public void setDepartmentFreshmanCourseList(
+			ArrayList<SelectItem> departmentFreshmanCourseList) {
+		this.departmentFreshmanCourseList = departmentFreshmanCourseList;
+	}
+//****************************************************************************
+//Dean Courses bu noktada bean e yükleniyor. Burada iþlem yok. 
+//Course.java da gerçekleþiyor
+	public ArrayList<SelectItem> getDeanCourseList() {
+		this.courseList = courseObj.getDeanCourses();
+		ArrayList<SelectItem> itemList = new ArrayList<SelectItem>();
+		for(int i = 0; i < this.courseList.size(); i++) {
+			String name = this.courseList.get(i).getCourseName();
+			itemList.add(new SelectItem(name));
+		}
+		this.deanCourseList = itemList;
+		return this.deanCourseList;
+	}
+
+	public void setDeanCourseList(ArrayList<SelectItem> deanCourseList) {
+		this.deanCourseList = deanCourseList;
+	}
+//*****************************************************************************
+// Seçilen dersin hocasýný gösteren listenin get set metodlarý
+	public ArrayList<SelectItem> getDeanLecturerList() {
+		if(deanLecturerList == null)
+		{		
+			loadLecturer();
+		}
+		return deanLecturerList;
+	}
+
+	public void setDeanLecturerList(ArrayList<SelectItem> deanLecturerList) {
+		this.deanLecturerList = deanLecturerList;
+	}
+//******************************************************************************	
+	public String getCreditValueTheo() {
+		return creditValueTheo;
+	}
+
+	public void setCreditValueTheo(String creditValueTheo) {
+		this.creditValueTheo = creditValueTheo;
+	}
+
+	public String getCreditValuePrac() {
+		return creditValuePrac;
+	}
+
+	public void setCreditValuePrac(String creditValuePrac) {
+		this.creditValuePrac = creditValuePrac;
+	}
+//*****************************************************
+// Seçili olan dekanlýk dersinin get-set metodlarý
+	public String getSelectedDeanCourse() {
+		return selectedDeanCourse;
+	}
+
+	public void setSelectedDeanCourse(String selectedDeanCourse) {
+		this.selectedDeanCourse = selectedDeanCourse;
+	}
+//*****************************************************
+	public String getSelectedDeanLecturer() {
+		return selectedDeanLecturer;
+	}
+
+	public void setSelectedDeanLecturer(String selectedDeanLecturer) {
+		this.selectedDeanLecturer = selectedDeanLecturer;
+	}
+	
+	public String[][] getInitCourseTable() {
+		return initCourseTable;
+	}
+
+	public void setInitCourseTable(String[][] initCourseTable) {
+		this.initCourseTable = initCourseTable;
+	}
+
+	public boolean isButtonStatus() {
+		return buttonStatus;
+	}
+
+	public void setButtonStatus(boolean buttonStatus) {
+		this.buttonStatus = buttonStatus;
+	}
+	
+	public String getSelectedRoom() {
+		return selectedRoom;
+	}
+
+	public void setSelectedRoom(String selectedRoom) {
+		this.selectedRoom = selectedRoom;
+	}
+
+	public String getSelectedOperation() {
+		return selectedOperation;
+	}
+
+	public void setSelectedOperation(String selectedOperation) {
+		this.selectedOperation = selectedOperation;
+	}
+
+	public String getSelectedStartHour() {
+		return selectedStartHour;
+	}
+
+	public void setSelectedStartHour(String selectedStartHour) {
+		this.selectedStartHour = selectedStartHour;
+	}
+
+	public String getSelectedEndHour() {
+		return selectedEndHour;
+	}
+
+	public void setSelectedEndHour(String selectedEndHour) {
+		this.selectedEndHour = selectedEndHour;
+	}
+
+	public String getSelectedDeanDay() {
+		return selectedDeanDay;
+	}
+
+	public void setSelectedDeanDay(String selectedDeanDay) {
+		this.selectedDeanDay = selectedDeanDay;
+	}
+
+	public Color getTestColor() {
+		return testColor;
+	}
+
+	public void setTestColor(Color testColor) {
+		this.testColor = testColor;
+	}
+	//*********************************************
+
+	//***********************************************
+}
---
+++ AutoSchedulingPart/src/entities/utility/MatrixCompare.java
@@ -0,0 +1,39 @@
+package entities.utility;
+
+import entities.utility.Index;
+import java.util.ArrayList;
+
+public class MatrixCompare {
+	private boolean result;
+	private ArrayList<Index> indexes;
+	
+	public MatrixCompare() {	}
+	
+	public MatrixCompare(int[][] mtrx1, int[][] mtrx2) {
+		ArrayList<Index> retList = new ArrayList<Index>();
+		int row = 0;
+		int col;
+		while(row < mtrx1.length) {
+			col = 0;
+			while(col < mtrx1[row].length) {
+				if(mtrx1[row][col] == mtrx2[row][col]) {
+					Index item = new Index(row, col);
+					retList.add(item);
+				}
+				col++;
+			}
+			row++;
+		}
+		this.indexes = retList;
+		this.result = true;
+		if(this.indexes.isEmpty() == true) { this.result = false; }
+	}
+	
+	public boolean getResult() {
+		return this.result;
+	}
+
+	public ArrayList<Index> getIndexes() {
+		return this.indexes;
+	}
+}
