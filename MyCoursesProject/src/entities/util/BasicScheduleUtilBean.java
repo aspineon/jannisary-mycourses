a16496ef292bb1370f8340b80189ddef69eb4dee
erdiapaydin@gmail.com <erdiapaydin@gmail.com@46466cab-7080-a701-ecfc-22f6a81af26d>
2011-05-02 22:12:24 +0000

    BasicScheduleUtilBean.java sınıfı eklendi

---
+++ MyCoursesProject/src/entities/util/BasicScheduleUtilBean.java
@@ -0,0 +1,80 @@
+package entities.util;
+
+public class BasicScheduleUtilBean {
+	private String courseName;
+	private int teoricHours;
+	private int practiceHours;
+	private String lecturerName;
+	private int classroomId;
+	private int syllabusId;
+	private String courseType;
+	private String courseTheoricOrPraticName;
+	private int timeofCourse;
+	private int hours;
+	
+	public String getCourseName() {
+		return courseName;
+	}
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+	public int getTeoricHours() {
+		return teoricHours;
+	}
+	public void setTeoricHours(int teoricHours) {
+		this.teoricHours = teoricHours;
+	}
+	public int getPracticeHours() {
+		return practiceHours;
+	}
+	public void setPracticeHours(int practiceHours) {
+		this.practiceHours = practiceHours;
+	}
+	public String getLecturerName() {
+		return lecturerName;
+	}
+	public void setLecturerName(String lecturerName) {
+		this.lecturerName = lecturerName;
+	}
+	
+	public int getClassroomId() {
+		return classroomId;
+	}
+	public void setClassroomId(int classroomId) {
+		this.classroomId = classroomId;
+	}
+	public int getSyllabusId() {
+		return syllabusId;
+	}
+	public void setSyllabusId(int syllabusId) {
+		this.syllabusId = syllabusId;
+	}
+	public String getCourseType() {
+		return courseType;
+	}
+	public void setCourseType(String courseType) {
+		this.courseType = courseType;
+	}
+	public String getCourseTheoricOrPraticName() {
+		return courseTheoricOrPraticName;
+	}
+	public void setCourseTheoricOrPraticName(String courseTheoricOrPraticName) {
+		this.courseTheoricOrPraticName = courseTheoricOrPraticName;
+	}
+	public int getTimeofCourse() {
+		return timeofCourse;
+	}
+	public void setTimeofCourse(int timeofCourse) {
+		this.timeofCourse = timeofCourse;
+	}
+	public int getHours() {
+		return hours;
+	}
+	public void setHours(int hours) {
+		this.hours = hours;
+	}
+	
+	
+	
+	
+}
