65c71219aa409c9e54155bda9bc1be8e1f187e0f
onurozey@gmail.com <onurozey@gmail.com@46466cab-7080-a701-ecfc-22f6a81af26d>
2011-05-04 20:09:56 +0000

    entities.utility updated.

--- AutoSchedulingPart/src/entities/utility/ScheduleAtomic.java
+++ AutoSchedulingPart/src/entities/utility/ScheduleAtomic.java
@@ -1,6 +1,7 @@
 package entities.utility;
 
 import entities.dao.Syllabus;
+import java.util.ArrayList;
 
 public class ScheduleAtomic {
 	private Syllabus syllabus;
@@ -8,6 +9,7 @@ public class ScheduleAtomic {
 	private String day;
 	private int startHour;
 	private int credit;
+	private ArrayList<Integer> knowledge;
 	
 	public ScheduleAtomic() {
 		
@@ -19,7 +21,40 @@ public class ScheduleAtomic {
 		this.day = pDay;
 		this.startHour = pStartHour;
 		this.credit = pCredit;
-		
+		this.knowledge = this.generateKnowledge(pCredit);
+	}
+	
+	private ArrayList<Integer> generateKnowledge(int credit) {
+		ArrayList<Integer> retList = new ArrayList<Integer>();
+		if(credit == 1) {
+			for(int i = 1; i < 41; i++) {
+				retList.add(i);
+			}
+			return retList;
+		}
+		if(credit == 2) {
+			for(int i = 1; i < 41; i++) {
+				if((i % 4) != 0) {
+					retList.add(i);
+				}
+			}
+			return retList;
+		}
+		if(credit == 3) {
+			for(int i = 1; i < 41; i++) {
+				if(((i % 4) != 3) && ((i % 4) != 0)) {
+					retList.add(i);
+				}
+			}
+		}
+		if(credit == 4) {
+			for(int i = 1; i < 41; i++) {
+				if((i % 4) == 1) {
+					retList.add(i);
+				}
+			}
+		}
+		return retList;
 	}
 //*************** GETTER-SETTER METHODS *************************************************
 	public Syllabus getSyllabus() {
@@ -60,5 +95,13 @@ public class ScheduleAtomic {
 
 	public void setCredit(int credit) {
 		this.credit = credit;
+	}
+
+	public ArrayList<Integer> getKnowledge() {
+		return knowledge;
+	}
+
+	public void setKnowledge(ArrayList<Integer> knowledge) {
+		this.knowledge = knowledge;
 	}
 }
