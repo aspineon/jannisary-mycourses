3da57cc83c57f7a497e6d9f64512cbd0e04b44a2
erhun.baycelik@gmail.com <erhun.baycelik@gmail.com@46466cab-7080-a701-ecfc-22f6a81af26d>
2011-05-04 01:29:32 +0000

    Schedule add işlemi için ScheduleBean oluşturuldu. Arayüzden çağrımadığı için faces-config dosyasına eklenmedi.

---
+++ MyCoursesProject/src/entities/business/ScheduleBean.java
@@ -0,0 +1,63 @@
+package entities.business;
+
+import entities.dao.Schedule;
+import entities.dao.Syllabus;
+import entities.util.BasicScheduleUtilBean;
+
+/**
+ * @author arshavin
+ *
+ */
+public class ScheduleBean {
+	
+	private Schedule schedule = new Schedule();
+	private String pCourseType;
+	private int pTimeOfCourse;
+	private int pHours;
+	private Syllabus pSyllabus = new Syllabus();
+	private int pSyllabusId;
+	
+	public void addSchedule(BasicScheduleUtilBean[][] firstGradeSchedule, BasicScheduleUtilBean[][] secondGradeSchedule,
+			BasicScheduleUtilBean[][] thirdGradeSchedule,BasicScheduleUtilBean[][] fourthGradeSchedule){
+		
+		schedule.setFirstGradeSchedule(firstGradeSchedule);
+		schedule.setSecondGradeSchedule(secondGradeSchedule);
+		schedule.setThirdGradeSchedule(thirdGradeSchedule);
+		schedule.setFourthGradeSchedule(fourthGradeSchedule);
+		
+		schedule.addSchedule();
+	}
+
+	public String getpCourseType() {
+		return pCourseType;
+	}
+
+	public void setpCourseType(String pCourseType) {
+		this.pCourseType = pCourseType;
+	}
+
+	public int getpTimeOfCourse() {
+		return pTimeOfCourse;
+	}
+
+	public void setpTimeOfCourse(int pTimeOfCourse) {
+		this.pTimeOfCourse = pTimeOfCourse;
+	}
+
+	public int getpHours() {
+		return pHours;
+	}
+
+	public void setpHours(int pHours) {
+		this.pHours = pHours;
+	}
+
+	public int getpSyllabusId() {
+		return pSyllabusId;
+	}
+
+	public void setpSyllabusId(int pSyllabusId) {
+		this.pSyllabusId = pSyllabusId;
+	}
+	
+}
