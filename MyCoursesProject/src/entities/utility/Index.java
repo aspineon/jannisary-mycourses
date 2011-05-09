cd113af13a1ae811a1b7842a07d34dd9d8ad410f
onurozey@gmail.com <onurozey@gmail.com@46466cab-7080-a701-ecfc-22f6a81af26d>
2011-05-09 16:07:23 +0000

    Index.java atomik nesnenin alt alanlarına göre düzenlendi.

--- MyCoursesProject/src/entities/utility/Index.java
+++ MyCoursesProject/src/entities/utility/Index.java
@@ -2,29 +2,45 @@ package entities.utility;
 
 public class Index {
 	
-	private int row;
-	private int column;
+	private String day = "";
+	private int hour = 0;
 	
 	public Index() {	}
 	
-	public Index(int rw, int cl) {
-		this.row = rw;
-		this.column = cl;
+	public Index(String pDay, int pHour) {
+		this.day = pDay;
+		this.hour = pHour;
 	}
 	
-	public int getRow() {
-		return this.row;
+	public Index(Index item) {
+		this.day = item.getDay();
+		this.hour = item.getHour();
 	}
 	
-	public void setRow(int rw) {
-		this.row = rw;
+	public Index(int row, int col) {
+		this.day = "";
+		if(row == 0) { this.day = "Monday"; }
+		if(row == 1) { this.day = "Tuesday"; }
+		if(row == 2) { this.day = "Wednesday"; }
+		if(row == 3) { this.day = "Thursday"; }
+		if(row == 4) { this.day = "Friday"; }
+		this.hour = col + 1; 
+	} 
+
+	public String getDay() {
+		return day;
 	}
-	
-	public int getColumn() {
-		return this.column;
+
+	public void setDay(String day) {
+		this.day = day;
 	}
-	
-	public void setColumn(int cl) {
-		this.column = cl;
+
+	public int getHour() {
+		return hour;
 	}
+
+	public void setHour(int hour) {
+		this.hour = hour;
+	}
+
 }
