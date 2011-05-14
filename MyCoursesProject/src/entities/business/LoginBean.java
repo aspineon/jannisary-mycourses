21a3ebc47cec2c4c5a1ca64903e7b29af882ffa7
fatihgundogdu89 <fatihgundogdu89@46466cab-7080-a701-ecfc-22f6a81af26d>
2011-05-14 18:38:05 +0000

    Yeni oluşturulan Login.jsp sayfasının Bean dosyası

---
+++ MyCoursesProject/src/entities/business/LoginBean.java
@@ -0,0 +1,57 @@
+package entities.business;
+
+import java.util.List;
+import javax.servlet.http.*;
+import entities.dao.SysUser;
+
+public class LoginBean 
+{
+	String userName;
+	String password;
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String checkLogin()
+	{
+		HttpServletResponse response = null;
+		SysUser sysUserObj = new SysUser();
+		List<SysUser> sysUserList;
+		
+		try
+		{
+			sysUserObj.setUserName(userName);
+			sysUserObj.setUserPassword(password);
+			
+			sysUserList = sysUserObj.getSysUserByUsernameAndPassword();
+			
+			if(sysUserList.size() == 0)
+			{
+				System.err.println("Login Fail");
+			}
+			else
+			{
+				System.out.println("Success");
+				response.sendRedirect(response.encodeRedirectUrl("http://localhost:8080/MyCoursesProject/faces/PreProd/index.jsp"));
+			}
+		
+		}
+		catch(Exception ex)
+		{
+			System.out.println(ex.getMessage());
+		}
+		return null;
+	}
+	
+	
+}
