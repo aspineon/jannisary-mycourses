e6c0f03444f848ffcdda4b18e2a9059ab129c81d
erhun.baycelik@gmail.com <erhun.baycelik@gmail.com@46466cab-7080-a701-ecfc-22f6a81af26d>
2011-05-18 18:19:37 +0000

    SysUser combobox ları için kodda düzenleme yapıldı. Issue 58  ile ilgili.

--- MyCoursesProject/src/entities/business/SysUserBean.java
+++ MyCoursesProject/src/entities/business/SysUserBean.java
@@ -9,11 +9,8 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-
 import javax.faces.event.ValueChangeEvent;
 import javax.faces.model.SelectItem;
-
-
 import entities.dao.SysUser;
 
 /**
@@ -27,8 +24,6 @@ public class SysUserBean {
 		selectItems.add(new SelectItem("Admin"));
 	}
 	
-	
-
 	/* Bug Fixed
 	 * Bug selectionChanged method***********
 	 * Su an bu olay en son değişikliği kaydediyor, o yüzden ekranda ekleme
@@ -37,38 +32,26 @@ public class SysUserBean {
 	 public void selectionChanged(ValueChangeEvent  evt) {
 		 String selectedValue = (String) evt.getNewValue();
 		 
-
 		 if (!selectedValue.equals("")) {
 			 currentItem.setUserStatus(selectedValue);
 		 }
 	}
 	
 	public List<SysUser> getAllUsers() {
-		synchronized (this) {
-			if (allUsers == null) {
-				allUsers = new ArrayList<SysUser>();
-					try {
-						allUsers = currentItem.getAllUser();
-						//listSeperator();
-					} catch (Exception e) {
-						System.out.println("!!!!!!loadAllUsers Error: "
-								+ e.getMessage());
-						e.printStackTrace();
-					}
+		synchronized (this) {	
+			allUsers = new ArrayList<SysUser>();
+			try {
+				allUsers = currentItem.getAllUser();
+			} catch (Exception e) {
+				System.out.println("!Load AllUsers Error: " + e.getMessage());
 			}
 		}
 		return allUsers;
 	}
-
 	
-
 	public String addUser(){
 		try{
-			
-			
 			int size = allUsers.size();		
-			
-			
 			SysUser sysUser = new SysUser(currentItem);
 			allUsers.add(size,sysUser);
 			sysUser.addUser();
@@ -78,19 +61,12 @@ public class SysUserBean {
 			currentItem.setUserName("");
 			currentItem.setUserPassword("");
 			allUsers = sysUser.getAllUser();
-			
-			
 		}catch(Exception ex){
-			System.err.println(ex.getMessage());}
+			System.out.println(ex.getMessage());}
 		return null;
 	}
 	
 	public void store() {
-		/*
-		allCars.set(currentRow, currentItem);
-		keys.clear();
-		keys.add(currentRow);
-		*/
 		
 		/*try-catch blogu eklenecek*/
 		try{
@@ -100,11 +76,10 @@ public class SysUserBean {
 			keys.clear();
 			keys.add(currentRow);
 		}catch(Exception ex){
-			System.err.println(ex.getMessage());
+			System.out.println(ex.getMessage());
 		}
 	}
 
-	
 	public void delete() {
 		
 		/* try-catch bloğu eklenecek
@@ -118,7 +93,7 @@ public class SysUserBean {
 			currentItem.deleteUser();
 			allUsers.remove(currentItem);
 		}catch(Exception ex){
-			System.err.println(ex.getMessage());
+			System.out.println(ex.getMessage());
 		}
 	}
 	
@@ -149,60 +124,40 @@ public class SysUserBean {
 	public void setKeys(Set<Integer> keys) {
 		this.keys = keys;
 	}
-
 	
 	public String getUserName() {
 		return userName;
 	}
 
-
-
-
+	
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
-
-
-
-
+	
 	public String getUserStatus() {
 		return userStatus;
 	}
-
-
-
-
+	
 	public void setUserStatus(String userStatus) {
 		this.userStatus = userStatus;
 	}
-
-
-
-
+	
 	public String getUserPassword() {
 		return userPassword;
 	}
-
-
-
-
+	
 	public void setUserPassword(String userPassword) {
 		this.userPassword = userPassword;
 	}
-
-
+	
 	private SysUser currentItem = new SysUser();
 	List<SelectItem> selectItems = new ArrayList<SelectItem>();
 	private Set<Integer> keys = new HashSet<Integer>();
 	private int currentRow;
-	
-	
 	private String userName;
 	private String userStatus;
 	private String userPassword;
-	
-	
-	private List<SysUser> allUsers = null;
+	private List<SysUser> allUsers = new ArrayList<SysUser>();
 }
 
 
