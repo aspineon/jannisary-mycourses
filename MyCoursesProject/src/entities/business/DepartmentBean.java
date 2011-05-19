ac79ec02efb7211c4ab643adb5b803495c8e7ff8
erhun.baycelik@gmail.com <erhun.baycelik@gmail.com@46466cab-7080-a701-ecfc-22f6a81af26d>
2011-05-19 17:00:29 +0000

    Kullanıcı kontrol işlemleri güncellendi.

--- MyCoursesProject/src/entities/business/DepartmentBean.java
+++ MyCoursesProject/src/entities/business/DepartmentBean.java
@@ -70,14 +70,6 @@ public class DepartmentBean {
 	}
 
 	public List<Department> getAllDepartments() {
-		/*if(!LoginBean.getLoginUser().getUserStatus().equals("admin")){
-	 		try {
-				FacesContext.getCurrentInstance().getExternalContext().redirect("http://localhost:8080/MyCoursesProject/faces/PreProd/login.jsp");
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-	 	}*/
 		synchronized (this) {
 			allDepartments = new ArrayList<Department>();
 			try {
